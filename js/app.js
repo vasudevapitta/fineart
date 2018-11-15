7ef639cf81c8f83e3296b0c17957a51edfc8827c
Vasudeva Pitta <38531141+vasudevapitta@users.noreply.github.com>
2018-11-15 15:35:57 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,10 +3,10 @@ const numOfImgs = 23;
 
 for(let i=1; i<=numOfImgs; i++){
 const anchor = document.createElement('a');
-anchor.href = 'images/'+i'.jpg';
+anchor.href = 'images/'+i+'.jpg';
 anchor.target = '_blank';
 const el = document.createElement('img');
-el.src = 'images/'+i'.jpg';
+el.src = 'images/'+i+'.jpg';
 el.alt = 'image of artwork';
 el.className = 'img';
 anchor.appendChild(el);
